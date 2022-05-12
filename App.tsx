92989246086b8602d6b2bbd476a47d3b6ae6b709
Nicolas <nicolasv0415@gmail.com>
2022-05-12 12:09:38 -0500

    init button

--- App.tsx
+++ App.tsx
@@ -1,11 +1,20 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, Button, Linking, View, ImageBackground, TouchableHighlight, Image } from 'react-native';
 
 export default function App() {
+  const url = 'https://baseproject.dy4pcxrzkp8qg.amplifyapp.com/';
+  const image = { uri: "https://i.ibb.co/mHvqYN9/Logos-low-fidelity-1.png" };
+
+  const OpenWeb = () => {
+    Linking.openURL(url);
+  };
+  //add coment init
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <TouchableHighlight onPress={OpenWeb}>
+        <Image style={styles.imagestyle} source={image} />
+      </TouchableHighlight>
     </View>
   );
 }
@@ -13,8 +22,19 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: '#000',
+    justifyContent: "center",
+    alignItems: "center",
   },
+  imagestyle: {
+    flex: 1,
+    justifyContent: "center",
+    height: 94,
+    width: 81,
+    borderRadius: 10,
+    resizeMode: 'stretch'
+  },
+  button: {
+    
+  }
 });
