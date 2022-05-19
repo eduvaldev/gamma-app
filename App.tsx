fad0dce50eb0fda220c2e5ceb2b9c1f908b7a328
Nicolas <nicolasv0415@gmail.com>
2022-05-19 09:28:00 -0500

    expo init web view

--- App.tsx
+++ App.tsx
@@ -1,9 +1,11 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, Button, Linking, View, ImageBackground, TouchableHighlight, Image } from 'react-native';
+import { StyleSheet, Linking, TouchableHighlight, Image, View } from 'react-native';
+import { LinearGradient } from 'expo-linear-gradient';
+import { WebView } from 'react-native-webview';
 
 export default function App() {
   const url = 'https://baseproject.dy4pcxrzkp8qg.amplifyapp.com/';
-  const image = { uri: "https://i.ibb.co/mHvqYN9/Logos-low-fidelity-1.png" };
+  const image = { uri: "https://i.ibb.co/rmQkpDv/Logos-low-fidelity-1-removebg.png" };
 
   const OpenWeb = () => {
     Linking.openURL(url);
@@ -11,30 +13,50 @@ export default function App() {
   //add coment init
 
   return (
-    <View style={styles.container}>
-      <TouchableHighlight onPress={OpenWeb}>
-        <Image style={styles.imagestyle} source={image} />
-      </TouchableHighlight>
-    </View>
+    // <LinearGradient
+    //   // Button Linear Gradient
+    //   colors={['#ffffff', '#DDE1FF']}
+    //   style={styles.container}>
+
+    //   <TouchableHighlight style={styles.contImage} onPress={OpenWeb}>
+    //     <Image style={styles.imagestyle} source={image} />
+    //   </TouchableHighlight>
+    //   <StatusBar style="dark" />
+    // </LinearGradient>
+    
+    <WebView 
+      style={styles.container}
+      source={{ uri: url }}
+    />
   );
 }
+
+// const styles = StyleSheet.create({
+//   container: {
+//     flex: 1,
+//     justifyContent: "center",
+//     alignItems: "center",
+//   },
+//   contImage: {
+//     height: 100,
+//     width: 100,
+//   },
+//   imagestyle: {
+//     flex: 1,
+//     justifyContent: "center",
+//     height: '100%',
+//     width: '100%',
+//     borderRadius: 30,
+//     resizeMode: 'stretch'
+//   },
+//   button: {
+
+//   }
+// });
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#000',
-    justifyContent: "center",
-    alignItems: "center",
+    marginTop: 30,
   },
-  imagestyle: {
-    flex: 1,
-    justifyContent: "center",
-    height: 94,
-    width: 81,
-    borderRadius: 10,
-    resizeMode: 'stretch'
-  },
-  button: {
-    
-  }
 });
